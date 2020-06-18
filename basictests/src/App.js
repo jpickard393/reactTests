b31ca08fbd705798d7f67b0e97e343dde06f6e5b
jpickard393 <jpickard@and.digital>
2020-06-18 16:30:26 +0100

    initial commit

---
+++ basictests/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from './Components/header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
